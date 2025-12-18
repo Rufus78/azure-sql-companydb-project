dcc4f1646a519997f84a9214042f9f3bf886ae68
Rufus78 <132641484+Rufus78@users.noreply.github.com>
2025-12-18 12:20:46 +0530

    Create insert_data.sql

---
+++ sql/insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees (FirstName, LastName, Department, Salary, JoiningDate)
+VALUES
+('Ravi', 'Kumar', 'IT', 45000, '2023-06-10'),
+('Anita', 'Sharma', 'HR', 40000, '2022-03-15'),
+('John', 'David', 'Finance', 50000, '2021-11-20'),
+('Priya', 'Singh', 'IT', 48000, '2023-01-05');
