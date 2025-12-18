efc396544d7d869b372b6cc76bd19409861b64bf
Rufus78 <132641484+Rufus78@users.noreply.github.com>
2025-12-18 12:21:12 +0530

    Update create_tables.sql

--- sql/sql/create_tables.sql
+++ sql/sql/create_tables.sql
@@ -1,1 +1,9 @@
+CREATE TABLE Employees (
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT,
+    JoiningDate DATE
+);
 
